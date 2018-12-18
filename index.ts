a559b2e40cd45c30acdd4ee684f1ffbdb6528ed8
Diego Majluf <diego.majluf@gmail.com>
2018-12-18 15:07:20 -0300

    consulta de folios

--- index.ts
+++ index.ts
@@ -14,12 +14,58 @@ export class ControlFolios {
             fieldName: string,
             fieldValue: string | number | boolean | ObjectId | Date
         }[]) {
+
+        return this.ultimoFolio(collectionName, fieldFolio, ...fieldNamesFilters).then(folio => ++folio)
+    }
+
+    async ultimoFolio(collectionName: string, fieldFolio: string, ...fieldNamesFilters:
+        {
+            fieldName: string,
+            fieldValue: string | number | boolean | ObjectId | Date
+        }[]) {
+
         if (!(collectionName in this.arbol)) this.arbol[collectionName] = {}
         if (!(fieldFolio in this.arbol[collectionName])) this.arbol[collectionName][fieldFolio] = {}
 
 
 
-        let ob = <{ promiseValue: Promise<number> }>fieldNamesFilters.reduce((acc, sa, i) => {
+        let ob = this.getFolioMemoryObject(collectionName, fieldFolio, ...fieldNamesFilters)
+
+        //El objecto es nuevo, es la primera vez que se pide el folio se consulta la base por única vez
+        if (ob.promiseValue == null) {
+            let filter = fieldNamesFilters.reduce((acc, va) => {
+                acc[va.fieldName] = va.fieldValue
+                return acc
+            }, <any>{})
+            ob.promiseValue = this.getFolioFromDb(filter, collectionName, fieldFolio, ...fieldNamesFilters)
+        }
+        /**
+         * La promesa ya está creada y tiene como valor el último folio entregado o 0 si es la primera vez
+         * Si aun no está resuelta se retorna el valor del folio siguiente una vez que se resuelva
+         * Si está resuelta se retorna el valor del folio siguiente inmediatamente
+        **/
+
+        return ob.promiseValue
+    }
+
+    resetFolio(collectionName: string, fieldFolio: string, ...fieldNamesFilters:
+        {
+            fieldName: string,
+            fieldValue: string | number | boolean | ObjectId | Date
+        }[]) {
+
+        let ob = this.getFolioMemoryObject(collectionName, fieldFolio, ...fieldNamesFilters)
+        delete ob.promiseValue
+
+    }
+
+    private getFolioMemoryObject(collectionName: string, fieldFolio: string, ...fieldNamesFilters:
+        {
+            fieldName: string,
+            fieldValue: string | number | boolean | ObjectId | Date
+        }[]) {
+
+        return <{ promiseValue: Promise<number> }>fieldNamesFilters.reduce((acc, sa, i) => {
             if (!(sa.fieldName in acc)) acc[sa.fieldName] = {}
             let fv = sa.fieldValue
             if (typeof fv == 'object' && 'getMonth' in fv) fv = fv.toJSON()
@@ -28,34 +74,26 @@ export class ControlFolios {
             if (!(fv in acc[sa.fieldName])) acc[sa.fieldName][fv] = {}
             return acc[sa.fieldName][fv]
         }, <any>this.arbol[collectionName][fieldFolio])
+    }
 
-        //El objecto es nuevo, es la primera vez que se pide el folio se consulta la base por única vez
-        if (ob.promiseValue == null) {
-            let filter = fieldNamesFilters.reduce((acc, va) => {
-                acc[va.fieldName] = va.fieldValue
-                return acc
-            }, <any>{})
-            ob.promiseValue = new Promise(async (res, rej) => {
-                let base = await this.db
-                res(base.collection(collectionName)
-                    .find(filter, { [fieldFolio]: 1 })
-                    .sort(fieldFolio, -1)
-                    .limit(1)
-                    .toArray()
-                    .then(x => x[0])
-                    .then(x => {
-                        if (x != null) return (x[fieldFolio] || 0)
-                        return 0
-                    }))
-            })
-        }
-        /**
-         * La promesa ya está creada y tiene como valor el último folio entregado o 0 si es la primera vez
-         * Si aun no está resuelta se retorna el valor del folio siguiente una vez que se resuelva
-         * Si está resuelta se retorna el valor del folio siguiente inmediatamente
-        **/
-
-        return ob.promiseValue = ob.promiseValue.then(folio => ++folio)
+    private getFolioFromDb(filter: any, collectionName: string, fieldFolio: string, ...fieldNamesFilters:
+        {
+            fieldName: string,
+            fieldValue: string | number | boolean | ObjectId | Date
+        }[]): Promise<number> {
+        return new Promise(async (res, rej) => {
+            let base = await this.db
+            res(base.collection(collectionName)
+                .find(filter, { [fieldFolio]: 1 })
+                .sort(fieldFolio, -1)
+                .limit(1)
+                .toArray()
+                .then(x => x[0])
+                .then(x => {
+                    if (x != null) return (x[fieldFolio] || 0)
+                    return 0
+                }))
+        })
     }
 }
 
