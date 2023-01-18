ac2874ef778f49e9d8eac6c8f96377713fd772db
Diego Majluf <DiegoMajluf@users.noreply.github.com>
2023-01-18 14:59:12 -0300

    no guardaba folio nuevo

--- index.ts
+++ index.ts
@@ -1,7 +1,12 @@
 import { Db, ObjectId } from "mongodb";
+type FieldNameFilter = {
+    fieldName: string,
+    fieldValue: string | number | boolean | ObjectId | Date
+}
 export class ControlFolios {
     private arbol: { [col: string]: { [field: string]: SubArbolType } } = {}
     constructor(protected db: Db | Promise<Db>) { }
+
 
     /**
      * Devuelve el folio siguiente para el control de folio especificado en los parámetros
@@ -9,49 +14,47 @@ export class ControlFolios {
      * @param fieldFolio Es el nombre del campo dentro de la colleción que se debe incrementar
      * @param fieldNamesFilters Un arreglo de campos con valor para los cuales se lleva un control del folio. Ej: idCliente
      */
-    async getNextFolio(collectionName: string, fieldFolio: string, ...fieldNamesFilters:
-        {
-            fieldName: string,
-            fieldValue: string | number | boolean | ObjectId | Date
-        }[]) {
+    async getNextFolio(collectionName: string, fieldFolio: string, ...fieldNamesFilters: FieldNameFilter[]) {
+        let ob = this.getUltimaPromesa(collectionName, fieldFolio, ...fieldNamesFilters)
+        return ob.promiseValue = ob.promiseValue.then(val => ++val)
 
-        return this.ultimoFolio(collectionName, fieldFolio, ...fieldNamesFilters).then(folio => ++folio)
     }
-    /**
-     * Devuelve el último folio asignado para el control de folio especificado en los parámetros
-     * @param collectionName Es el nombre de la colleción en la base de datos que contiene el folio
-     * @param fieldFolio Es el nombre del campo dentro de la colleción que se debe incrementar
-     * @param fieldNamesFilters Un arreglo de campos con valor para los cuales se lleva un control del folio. Ej: idCliente
-     */
-    ultimoFolio(collectionName: string, fieldFolio: string, ...fieldNamesFilters:
-        {
-            fieldName: string,
-            fieldValue: string | number | boolean | ObjectId | Date
-        }[]) {
 
-        if (!(collectionName in this.arbol)) this.arbol[collectionName] = {}
-        if (!(fieldFolio in this.arbol[collectionName])) this.arbol[collectionName][fieldFolio] = {}
+    /**
+ * Devuelve el último folio asignado para el control de folio especificado en los parámetros
+ * @param collectionName Es el nombre de la colleción en la base de datos que contiene el folio
+ * @param fieldFolio Es el nombre del campo dentro de la colleción que se debe incrementar
+ * @param fieldNamesFilters Un arreglo de campos con valor para los cuales se lleva un control del folio. Ej: idCliente
+ */
+    ultimoFolio(collectionName: string, fieldFolio: string, ...fieldNamesFilters: FieldNameFilter[]) {
+
+        return this.getUltimaPromesa(collectionName, fieldFolio, ...fieldNamesFilters).promiseValue
+    }
+
+    private getUltimaPromesa(collectionName: string, fieldFolio: string, ...fieldNamesFilters: FieldNameFilter[]): { promiseValue: Promise<number> } {
+
+        this.arbol[collectionName] ||= {}
+        this.arbol[collectionName][fieldFolio] ||= {}
 
 
 
         let ob = this.getFolioMemoryObject(collectionName, fieldFolio, ...fieldNamesFilters)
 
         //El objecto es nuevo, es la primera vez que se pide el folio se consulta la base por única vez
-        if (ob.promiseValue == null) {
-            let filter = fieldNamesFilters.reduce((acc, va) => {
-                acc[va.fieldName] = va.fieldValue
-                return acc
-            }, <any>{})
-            ob.promiseValue = this.getFolioFromDb(filter, collectionName, fieldFolio, ...fieldNamesFilters)
-        }
+        ob.promiseValue ||= this.getFolioFromDb(fieldNamesFilters.reduce((acc, va) => {
+            acc[va.fieldName] = va.fieldValue
+            return acc
+        }, {}), collectionName, fieldFolio, ...fieldNamesFilters)
+
         /**
-         * La promesa ya está creada y tiene como valor el último folio entregado o 0 si es la primera vez
+         * La promesa ya está creada y tiene como valor el último folio entregado o 0 si es la primera vez.
          * Si aun no está resuelta se retorna el valor del folio siguiente una vez que se resuelva
          * Si está resuelta se retorna el valor del folio siguiente inmediatamente
         **/
 
-        return ob.promiseValue
+        return <{ promiseValue: Promise<number> }>ob
     }
+
 
     /**
      * Elimina de la memoria el registro de folios asociados al control especificado en los parámetros
@@ -59,51 +62,36 @@ export class ControlFolios {
      * @param fieldFolio Es el nombre del campo dentro de la colleción que se debe incrementar
      * @param fieldNamesFilters Un arreglo de campos con valor para los cuales se lleva un control del folio. Ej: idCliente
      */
-    resetFolio(collectionName: string, fieldFolio: string, ...fieldNamesFilters:
-        {
-            fieldName: string,
-            fieldValue: string | number | boolean | ObjectId | Date
-        }[]) {
+    resetFolio(collectionName: string, fieldFolio: string, ...fieldNamesFilters: FieldNameFilter[]) {
 
         let ob = this.getFolioMemoryObject(collectionName, fieldFolio, ...fieldNamesFilters)
         delete ob.promiseValue
 
     }
 
-    private getFolioMemoryObject(collectionName: string, fieldFolio: string, ...fieldNamesFilters:
-        {
-            fieldName: string,
-            fieldValue: string | number | boolean | ObjectId | Date
-        }[]) {
+    private getFolioMemoryObject(collectionName: string, fieldFolio: string, ...fieldNamesFilters: FieldNameFilter[]) {
 
         return <{ promiseValue?: Promise<number> }>fieldNamesFilters.reduce((acc, sa, i) => {
-            if (!(sa.fieldName in acc)) acc[sa.fieldName] = {}
+            acc[sa.fieldName] ||= {}
             let fv = sa.fieldValue
             if (typeof fv == 'object' && 'getMonth' in fv) fv = fv.toJSON()
             else if (typeof fv == 'object') fv = fv.toHexString()
             fv = fv.toString()
-            if (!(fv in acc[sa.fieldName])) acc[sa.fieldName][fv] = {}
+            acc[sa.fieldName][fv] ||= {}
             return acc[sa.fieldName][fv]
-        }, <any>this.arbol[collectionName][fieldFolio])
+        }, this.arbol[collectionName][fieldFolio])
     }
 
-    private getFolioFromDb(filter: any, collectionName: string, fieldFolio: string, ...fieldNamesFilters:
-        {
-            fieldName: string,
-            fieldValue: string | number | boolean | ObjectId | Date
-        }[]): Promise<number> {
+    private getFolioFromDb(filter: any, collectionName: string, fieldFolio: string, ...fieldNamesFilters: FieldNameFilter[]): Promise<number> {
         return new Promise(async (res, rej) => {
             let base = await this.db
             res(base.collection(collectionName)
                 .find(filter, { [fieldFolio]: 1 })
                 .sort(fieldFolio, -1)
-                .limit(1)
+                .limit(-1)
                 .toArray()
                 .then(x => x[0])
-                .then(x => {
-                    if (x != null) return (x[fieldFolio] || 0)
-                    return 0
-                }))
+                .then(x => x && x[fieldFolio] || 0))
         })
     }
 }
